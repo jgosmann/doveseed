da446a4b12afaa632142d89fdd10289205dfc019
Jan Gosmann <jan@hyper-world.de>
2019-11-23 18:49:35 +0100

    Fix ReCaptchaMiddleware

--- doveseed/recaptcha.py
+++ doveseed/recaptcha.py
@@ -6,9 +6,14 @@ from urllib.parse import urlencode
 import re
 
 import werkzeug.wrappers
+import werkzeug.wrappers.json
 
 
 Logger = logging.getLogger(__name__)
+
+
+class JsonRequest(werkzeug.wrappers.json.JSONMixin, werkzeug.wrappers.Request):
+    pass
 
 
 class ReCaptchaMiddleware:
@@ -21,18 +26,28 @@ class ReCaptchaMiddleware:
         self._secret = config["secret"]
 
     def __call__(self, environ, start_response):
-        request = werkzeug.wrappers.Request(environ)
-        if self.paths.match(request.path):
-            if not self.verify_captcha(
-                request.get_json()["captcha"], request.remote_addr
-            ):
-                return "Invalid captcha.", http.HTTPStatus.UNAUTHORIZED
+        Logger.error("start")
+        request = JsonRequest(environ)
+        if self.paths.match(request.path) and request.method == "POST":
+            data = request.get_json()
+            if data is None or "captcha" not in data:
+                return self._to_response(
+                    environ,
+                    start_response,
+                    ("Missing captcha.", http.HTTPStatus.BAD_REQUEST),
+                )
+            if not self.verify_captcha(data["captcha"], request.remote_addr):
+                return self._to_response(
+                    environ,
+                    start_response,
+                    ("Invalid captcha.", http.HTTPStatus.UNAUTHORIZED),
+                )
         return self.app(environ, start_response)
 
     def verify_captcha(self, captcha: str, remote_ip: str):
         verification_request = urllib.request.Request(
             "https://www.google.com/recaptcha/api/siteverify",
-            method="post",
+            method="POST",
             data=urlencode(
                 {"secret": self._secret, "response": captcha, "remoteip": remote_ip}
             ).encode("ascii"),
@@ -42,3 +57,6 @@ class ReCaptchaMiddleware:
         if len(result.get("error-codes", [])) > 0:
             Logger.warning(result["error-codes"])
         return result["success"] and result["hostname"] in self.valid_hostnames
+
+    def _to_response(self, environ, start_response, response):
+        return werkzeug.wrappers.Response(*response)(environ, start_response)
